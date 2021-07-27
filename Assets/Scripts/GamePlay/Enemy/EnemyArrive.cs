79ed2d6a2bc3479912aae5f26afe0e39fb59714a
GalStilkol <Stilkolgal@gmail.com>
2021-07-27 13:38:49 +0300

    fixed enemies overlapping.

--- Assets/Scripts/GamePlay/Enemy/EnemyArrive.cs
+++ Assets/Scripts/GamePlay/Enemy/EnemyArrive.cs
@@ -25,8 +25,34 @@ public class EnemyArrive : MonoBehaviour
 
     void Arrive()
     {
+        int neighborCount=0;
+        Vector3 force= new Vector3(0,0,0);
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 1);
+        foreach (var collider in colliders)
+        {
+            if(collider.gameObject.GetComponent<EnemyArrive>()!=null)
+            {
+                force.x += collider.transform.position.x - transform.position.x;
+                force.z += collider.transform.position.z - transform.position.z;
+                neighborCount++;
+            }
+        }
+        Debug.Log("forceX is = " + force.x);
+        Debug.Log("forceZ is = " + force.z);
+        if (neighborCount != 0)
+        {
+            force.x /= neighborCount;
+            force.z /= neighborCount;
+
+            force = Vector3.Scale(force, new Vector3(-1, -1, -1));
+        }
+
+        force.Normalize();
+        force = Vector3.Scale(force,new Vector3 (2,2,2));
+
         Vector3 direction = target.position - transform.position;
         direction.y = 0;
+        direction = direction + force;
         float distance = direction.magnitude;
         float decelerationFactor = distance / 5;
         float speed = moveSpeed * decelerationFactor;
